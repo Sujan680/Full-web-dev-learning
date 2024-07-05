58bcac79e90b431ef6e4e8a1a09c0326532f6705
sujan <budhasujan3@gmail.com>
2024-07-05 18:13:20 +0545

    Day 10 of javascript intro

---
+++ Day 10 Javascript/script.js
@@ -0,0 +1,19 @@
+alert("Hello world");
+
+console.log("Code is running...");
+
+console.log("HI my name is sujan");
+
+// var a = prompt("Enter Your name");
+
+console.log("Your name is",a);
+
+var b = 20;
+
+if(b > 18){
+    console.log("Not eligible to vote");
+}else{
+console.log("Eligible to vote");
+}
+
+document.body.style.backgroundColor = "blue";
