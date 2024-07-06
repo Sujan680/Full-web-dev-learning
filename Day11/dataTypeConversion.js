2e32573a607f23ae2a1737fcb52757cfc67fd6b7
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-07-06 10:58:27 +0000

    typeconversion and opertions in js

--- Day11/dataTypeConversion.js
+++ Day11/dataTypeConversion.js
@@ -4,22 +4,22 @@ console.log("Hi this is data type conversion");
 // let score =null;
 // let score = undefined;
 // let score = true;
-let score = 'sujan';
+// let score = 'sujan';
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInnumber = Number(score);
-console.log(valueInnumber);
+// let valueInnumber = Number(score);
+// console.log(valueInnumber);
 
-console.log(typeof valueInnumber);
+// console.log(typeof valueInnumber);
 
 // let num =34543;
 // let num = null;
 let num = undefined;
 
-let valueInString = String(num);
-console.log(valueInString);
-console.log(typeof valueInString);
+// let valueInString = String(num);
+// console.log(valueInString);
+// console.log(typeof valueInString);
 
 // 33 => 33
 // "33afd" => NaN
@@ -27,7 +27,50 @@ console.log(typeof valueInString);
 
 // let isloggedIn = 1; //true
 // let isloggedIn = 0;  // flse
-// let isloggedIn = ""; // false
+ let isloggedIn = ""; // false
 // let isloggedIn = "suna"; //true
-let booleanIsloggedIn =Boolean(isloggedIn)
-console.log(booleanIsloggedIn);
+// let booleanIsloggedIn =Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+
+// Operations...............
+
+let value = 4;
+let nebgValue = -value;
+console.log(nebgValue);
+
+console.log(90%6);
+
+let str1 = "sujan";
+let str2 = "magar";
+
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 3);
+
+console.log("1" + 2 + "3");
+console.log(1 + 2 + "3");
+console.log("1" + "3" + 3);
+
+
+//If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+let x =3;
+let y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+//If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+
+
+
+
+
+// string to number conversion?
