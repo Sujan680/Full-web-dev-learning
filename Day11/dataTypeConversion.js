acf62c42ec89f8657d285bad29e13609decb74e7
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-07-06 10:36:19 +0000

    type conversion in js

---
+++ Day11/dataTypeConversion.js
@@ -0,0 +1,33 @@
+console.log("Hi this is data type conversion");
+
+// let score = "dfs333";
+// let score =null;
+// let score = undefined;
+// let score = true;
+let score = 'sujan';
+
+console.log(typeof score);
+
+let valueInnumber = Number(score);
+console.log(valueInnumber);
+
+console.log(typeof valueInnumber);
+
+// let num =34543;
+// let num = null;
+let num = undefined;
+
+let valueInString = String(num);
+console.log(valueInString);
+console.log(typeof valueInString);
+
+// 33 => 33
+// "33afd" => NaN
+// true =1 / false = 0
+
+// let isloggedIn = 1; //true
+// let isloggedIn = 0;  // flse
+// let isloggedIn = ""; // false
+// let isloggedIn = "suna"; //true
+let booleanIsloggedIn =Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
