464fd3feaca72623b09eda064b820025be199054
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-07-06 11:33:28 +0000

    datatypes-summary in js

---
+++ Day11/datatypes-summary.js
@@ -0,0 +1,38 @@
+// primitive data types 7 types
+
+// Number
+// String
+// Boolean
+// null
+// undefined
+// BigInt
+// Symbol
+
+let score = 202;
+let str = "sujan";
+let userEmail;
+let isLoogedIn = true;
+
+let id = Symbol('233');
+let another = Symbol('233');
+console.log(id = another);
+
+const bigNmber = 234434343454334n;
+
+//Reference type/non primitives
+// Object
+// Array
+// Function
+
+const heros = ["sujan", "Magar", "NEpal"]
+
+let obj = {
+    name:"suana",
+    age:34
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
