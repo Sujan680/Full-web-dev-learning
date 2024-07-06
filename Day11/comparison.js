ad2031e8ab68f5a4dae63036be55cc5e8ebebfd5
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-07-06 11:17:06 +0000

    comparison in js

---
+++ Day11/comparison.js
@@ -0,0 +1,21 @@
+// console.log(4<10);
+// console.log(4<=10);
+// console.log(4>=10);
+// console.log(4>10);
+// console.log(4==10);
+// console.log(4!=10);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null == 0);
+console.log(null === 0);
+console.log(null >= 0);
+console.log(null > 0);
+
+
+// ===	equal value and equal type
+console.log(4===6);
+
+console.log("3"==3);
