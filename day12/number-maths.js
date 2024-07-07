0f3806fb01b685aad454d3ec5e021eca6e43e52f
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-07-07 09:37:04 +0000

    Number and Maths in Js

---
+++ day12/number-maths.js
@@ -0,0 +1,44 @@
+console.log("Number and Maths in Js");
+
+// const score = new Number(23);
+// console.log(score);
+
+// // console.log(score.toExponential(2));
+
+// // console.log(typeof(score.toString()));
+
+
+// console.log(score.toFixed(3)); //23.000
+
+// console.log(score.toPrecision(2)); //23
+
+// console.log(score.toLocaleString());
+
+// const num = 34.463;
+// console.log(num.toPrecision(2));
+
+// const hundreds = 100000;
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//*******************Maths*********** */
+
+// console.log(Math);
+
+// console.log(Math.abs(-3));
+
+// console.log(Math.round(34.464));
+
+// console.log(Math.ceil(34.7));
+// console.log(Math.floor(34.7));
+
+// console.log(Math.max(2,5,7,10));
+// console.log(Math.min(2,5,7,10));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+console.log(Math.trunc(3.3));
+console.log(Math.sign(-3.3));
