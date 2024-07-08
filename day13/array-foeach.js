8906108880417612ee2b58b6765502f34f2d19d6
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-07-08 09:06:41 +0000

    Js Array methods for each, some, every() , Arry.from() , keys(), entries()..

--- day13/array-foeach.js
+++ day13/array-foeach.js
@@ -29,3 +29,38 @@ letters.forEach(item => {
     }
 })
 console.log(count);
+
+
+// Js Array some() methods
+// The some() method checks if some array values pass a test.
+
+const number = [45, 4, 9, 16, 25];
+let someOver18 = number.some(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
+console.log(someOver18);
+
+// The every() method checks if all array values pass a test.
+const number2 = [45, 4, 9, 16, 25];
+let someOver16 = number2.every(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 16;
+}
+console.log(someOver16);
+
+
+const string = "ABCDEFG";
+const myArr = Array.from(string);
+console.log(myArr);
+
+const keys = number2.keys();
+for (const i of keys) {
+    console.log(i);
+}
+const index = number2.entries();
+for (const i of index) {
+    console.log(i);
+}
