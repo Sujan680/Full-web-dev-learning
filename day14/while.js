7b4d0270e446a40bd2044171482c9711fb273408
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-07-09 05:06:23 +0000

    while do-while loop in Js

---
+++ day14/while.js
@@ -0,0 +1,22 @@
+let index = 0;
+while (index <= 10) {
+    console.log(`Value of index is`, index);
+    index+=2;
+}
+
+
+const myArray = ["sujan", "Magar", "Yamaha"];
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(`My array is`, myArray[arr]);
+    arr+=1;
+}
+
+
+
+// do while loop
+let score = 1;
+do {
+    console.log(`The score is ${score}`);
+    score+=1;
+} while (score < 10);
