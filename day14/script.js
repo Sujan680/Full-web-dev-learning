16585625bd1bac9461ca55e6e6b8fed0ae6c91e8
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-07-09 17:41:22 +0000

    ChildNodes selection in Js

---
+++ day14/script.js
@@ -0,0 +1,50 @@
+console.log("Hello wolrd");
+
+// selecting childNodes in JS
+
+// document.body
+// <body cz-shortcut-listen=​"true">​…​</body>​<div class=​"container">​…​</div>​<script src=​"script.js">​</script>​<!-- Code injected by live-server --><script>​…​</script>​</body>​
+// document.body.childNodes
+// NodeList(9) [text, div.container, text, script, text, comment, text, script, text]
+// document.body.childNodes[0]
+// #text
+// document.body.childNodes[2]
+// #text
+// document.body.childNodes[1]
+// <div class=​"container">​…​</div>
+// ​<div class=​"box">​1​</div>
+// ​<div class=​"box">​2​</div>
+// ​<div class=​"box">​3​</div>
+// ​<div class=​"box">​4​</div>
+// ​<div class=​"box">​5​</div>
+// ​</div>​
+// document.body.childNodes[1].childNodes[1]
+// <div class=​"box">​1​</div>​
+// document.body.childNodes[1].childNodes[2]
+// #text
+// document.body.childNodes[1].childNodes[3]
+// <div class=​"box">​2​</div>​
+// let cont = document.body.childNodes[1]
+// undefined
+// cont
+// <div class=​"container">​…​</div>​
+// cont.firstChild
+// #text
+// cont.lastChild
+// #text
+// cont.firstElementChild
+// <div class=​"box">​1​</div>​
+// cont.lastElementChild
+// <div class=​"box">​5​</div>​
+// cont.lastElementChild.style.backgroundColor = "green"
+// 'green'
+// cont.firstElementChild.backgroundColor = "blue"
+// 'blue'
+// cont.firstElementChild.style.backgroundColor = "blue"
+// 'blue'
+
+// parent node 
+// cont.firstElementChild.parentElement
+// <div class=​"container">​…​</div>​
+// cont.firstElementChild.parentElement.style.background = 'yellow'
+
