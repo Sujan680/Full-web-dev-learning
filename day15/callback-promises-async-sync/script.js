9b2c1e69a1021f699e2a13bab7bc86678cbe7b05
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-07-10 11:25:58 +0000

    Synchronous and asynchronous concept and callback function in js

---
+++ day15/callback-promises-async-sync/script.js
@@ -0,0 +1,46 @@
+// Synchronous means the code runs in a particular sequence of instrunctons given in the program , each instructions waits for the previous instructions to complete its execution.
+
+console.log("One");
+console.log("Two");
+console.log("Three");
+
+
+
+// Asynchronous concept
+// With asynchronous programming, JavaScript programs can start long-running tasks, and continue running other tasks in parallel.
+
+console.log("Hi am async");
+console.log("Hi am async 1");
+setTimeout( ()=> {
+    console.log("Hello");
+}, 3000)
+
+console.log("FOur");
+console.log("FIve");
+
+// Callback : is a function passed as argument to another function
+
+function sum(a,b){
+    console.log(a+b);
+}
+
+function calculator(a,b, sumCallback) {
+    sumCallback(a,b)
+}
+
+calculator(4,4,sum);
+
+function add(a,b){
+    return a+b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function calculator(a,b, operationCallback){
+    return operationCallback(a,b);
+}
+
+console.log(calculator(8,2, sum));
+console.log(calculator(8,2, divide));
