1a822f1b8936154821c74e9cf7dab267033d186d
sujan <budhasujan3@gmail.com>
2024-07-14 15:25:17 +0545

    day19 of js with events

---
+++ day19/script.js
@@ -0,0 +1,19 @@
+let btn = document.getElementById("btn")
+
+// btn.addEventListener("click", () => {
+//     document.querySelector(".box").innerHTML = "<b></b>You are changed</b> to Sujan Magar "
+// })
+
+btn.addEventListener("dblclick", () => {
+    document.querySelector(".box").innerHTML = "<b></b>You are changed</b> to Sujan Magar "
+})
+
+
+// document.addEventListener("keydown", (e) => {
+//     document.querySelector("#para").innerHTML = e.key
+//     console.log(e.key);
+// })
+
+
+// Event bubbling in js
+
